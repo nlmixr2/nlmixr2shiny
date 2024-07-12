c4aa79e3c28b915baf9cf6e7881a30f924b02d4e
Matthew Fidler <matthew.fidler@gmail.com>
2024-07-12 10:16:08 -0500

    Add draft function

---
+++ R/createShinyPKModel.R
@@ -0,0 +1,68 @@
+#' This function creates a nlmixr2 model by specifying the absorption, distribution and elimination
+#' 
+#' In general this is used in shiny to create the base PK model
+#'
+#' @param absorption This specifies the absorption method and can be:
+#'   - IV/Infusion/Bolus -- no changes to the base model
+#'   - First Order -- adds a depot compartment
+#'   - Transit -- adds a transit compartment of size `ntransit`
+#' @param distribution number of compartments in the model;  Can be:
+#' - 1 compartment
+#' - 2 compartment
+#' - 3 compartment
+#' @param elimination the type of elimination of PK.  Currently supports:
+#' - linear
+#' - Michaelis Menton
+#' @param ntransit number of transit compartments (by default 3)
+#' @param linCmt boolean, is this going to use a linear compartment solution
+#'
+#' @return a PK model according to the specifications above (evaluated not function)
+#' @export
+#'
+#' @examples
+#' 
+#' # By defualt this creates a one comparmtent ODE model from the `nlmixr2lib` model library
+#' createShinyPKModel()
+#' 
+#' createShinyPKModel(absorption="First Order")
+createShinyPKModel <- function(absorption=c("IV/Infusion/Bolus",
+                                            "First Order",
+                                            "Transit"), 
+                               distribution=c("1 compartment",
+                                              "2 compartment",
+                                              "3 compartment"), 
+                               elimination=c("linear",
+                                             "Michealis-Menton"), 
+                               ntransit=3,
+                               linCmt=FALSE,
+                               lag=FALSE) {
+  absorption <- match.arg(absorption)
+  distribution <- match.arg(distribution)
+  elimination <- match.arg(elimination)
+  checkmate::assertIntegerish(ntransit, lower=1, any.missing=FALSE, len=1)
+  checkmate::assertLogical(linCmt, any.missing = FALSE, len = 1)
+  ini <- rxode2::ini
+  if (!linCmt) {
+    .lib <- loadNamespace("nlmixr2lib")
+    .f <- switch(distribution,
+                "1 compartment"=nlmixr2lib::readModelDb("PK_1cmt_des"),
+                "2 compartment"=nlmixr2lib::readModelDb("PK_2cmt_des"),
+                "3 compartment"=nlmixr2lib::readModelDb("PK_3cmt_des"))
+    .f <- .f()
+    .f <- nlmixr2lib::removeDepot(.f)
+    if (absorption == "First Order") {
+      .f <- nlmixr2lib::addDepot(.f,lag = lag)
+      .f <- ini(.f, lfdepot=fix(1))
+    } else {
+      if (absorption == "Transit") {
+        .f <- nlmixr2lib::addDepot(.f, lag = FALSE)
+        .f <- ini(.f, lfdepot=fix(1))
+        .f <- nlmixr2lib::addTransit(.f, transit = ntransit)
+      }
+    }
+    
+  } else {
+    stop("not implented", call.=FALSE)
+  }
+  .f
+}
---
+++ R/reexport.R
@@ -0,0 +1,12 @@
+#' @importFrom rxode2 ini
+#' @export
+rxode2::ini
+
+#' @importFrom rxode2 model
+#' @export
+rxode2::model
+
+#' @importFrom lotri lotri
+#' @export
+lotri::lotri
+
