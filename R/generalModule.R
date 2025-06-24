58c05cff44303bc329aa98d5e11f1e13fcd71866
Dyani99 <dyani1115@gmail.com>
2025-06-24 21:30:37 +0200

    Added the calculating statistical model function  and the calculating parameter estimate model function.

--- R/generalModule.R
+++ R/generalModule.R
@@ -1,4 +1,65 @@
-#' The nlmixr2model function
+#' This function is supposed to calculate the initial model.
+#'
+#' @param results The Shiny lists that contains the results.
+#'
+#' @return nothing called for side effects 
+#' @noRd
+#'
+#' 
+calculatingInitialModel <-function(results){
+  if (is.null(results$pkpdm)){
+    
+    waiter_show(html = tagList(
+      spin_fading_circles(),  # A nice spinning loading indicator
+      h4("Calculating initial model...")
+    ))
+    
+    # Evaluate pipeline for results$pkpdm
+    
+    results$pkpdm <- eval(str2lang(results$pkpdpipe))
+    
+    waiter_hide()
+  }
+}
+
+#' This function is supposed to calculate the statistical model.
+#'
+#' @param results This Shiny list contains the results.
+#'
+#' @return nothing called for side effects 
+#' @noRd
+#'
+#' 
+calculatingStatisticalModel <-function(results){
+  if (is.null(results$forCov)){
+    waiter_show(html = tagList(
+      spin_fading_circles(),  # A nice spinning loading indicator
+      h4("Calculating statistical model...")
+    ))
+    
+    # Evaluate the pipeline for results$forCov
+    results$forCov <- eval(str2lang(paste(c("results$parEstim", results$ParEstimates), collapse = "|>\n\t")))
+    
+    waiter_hide()
+  }
+}
+
+calculatingParameterEstimate <-function(results) {
+  if (is.null(results$ParaEstim)) {
+    waiter_show(html = tagList(
+      spin_fading_circles(),  # A nice spinning loading indicator
+      h4("Calculating parameter estimates...")
+    ))
+    
+    # Evaluate the pipeline for results$ParaEstim
+    results$parEstim <- eval(str2lang(paste(c("results$pkpdm", results$modProp), collapse = "|>\n\t")))
+    
+    waiter_hide()
+  }
+  
+}
+#' The nlmixr2model function 
+#'
 #'
 #' @description The main UI function for the nlmixr2Shiny app using Shiny (no miniUI).
 #'
@@ -79,42 +140,38 @@ nlmixr2model <- function() {
     # Monitor active tab and update results based on the selected tab
     observeEvent(input$mainTabs, {
       tab <- input$mainTabs
-
+      
       if (tab == "Model Property") {
-        waiter_show(html = tagList(
-          spin_fading_circles(),  # A nice spinning loading indicator
-          h4("Calculating, please wait...")
-        ))
-
-        # Evaluate pipeline for results$pkpdm
-        results$pkpdm <- eval(str2lang(results$pkpdpipe))
-        waiter_hide()
+        
       }
+       
 
       if (tab == "PKPD Model") {
 
         results$modProp <- NULL
         results$pkpdm <- NULL
       } else if (tab == "Model Property") {
+        calculatingInitialModel(results) 
         results$parEst <- NULL
       } else if (tab == "Parameter Estimate") {
-        waiter_show(html = tagList(
-          spin_fading_circles(),  # A nice spinning loading indicator
-          h4("Calculating, please wait...")
-        ))
+        calculatingInitialModel(results) 
+        calculatingParameterEstimate(results)
+        results$ParaEstim <- NULL
+        
         req(results$pkpdm)
-        results$parEstim <- eval(str2lang(paste(c("results$pkpdm", results$modProp), collapse = "|>\n\t")))
+       
         waiter_hide()
       } else if (tab == "Statistical Model") {
-        waiter_show(html = tagList(
-          spin_fading_circles(),  # A nice spinning loading indicator
-          h4("Calculating, please wait...")
-        ))
+        calculatingInitialModel(results) 
+        calculatingStatisticalModel(results)
+        results$forCov <- NULL
+     
         req(results$parEstim)
-        results$forCov <- eval(str2lang(paste(c("results$parEstim", results$ParEstimates), collapse = "|>\n\t")))
+       
         waiter_hide()
       }
     })
+    
   }
 
   shiny::runGadget(ui, server, viewer = shiny::dialogViewer(
