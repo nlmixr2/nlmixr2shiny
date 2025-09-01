d82f7c6c8d9d955f7c2fe2185f5b40a56eed7953
Matthew Fidler <matthew.fidler@gmail.com>
2025-08-31 20:40:52 -0500

    More check fixes

--- R/pkpdfunctions.R
+++ R/pkpdfunctions.R
@@ -148,8 +148,8 @@ PDph <- function(response_type = c("Direct/Immediate", "Indirect/Turnover", "Eff
 
 
 jPh <- function(pkO,pdO){
-  checkmate::isCharacter(pkO, len = 1, any.missing = FALSE)
-  checkmate::isCharacter(pdO, len = 1, any.missing = FALSE)
+  checkmate::assertCharacter(pkO, len = 1, any.missing = FALSE)
+  checkmate::assertCharacter(pdO, len = 1, any.missing = FALSE)
   joined <- c(pkO,pdO)
   joined <- joined[joined!=""]
   joined <- paste(joined,collapse = "|>\n\t")
