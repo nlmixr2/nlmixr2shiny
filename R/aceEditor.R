2dcfec657959d41421f487d38fbca9fff91946a1
Matthew Fidler <matthew.fidler@gmail.com>
2025-09-17 10:13:53 -0500

    Add copy code for nlmixr2shiny

--- R/aceEditor.R
+++ R/aceEditor.R
@@ -21,56 +21,83 @@ aceServer <- function(id, results) {
                                 editorId="ace",
                                 value = editorContent())
     })
+    observeEvent(input$insertCode, {
+      req(input$ace)  # Ensure input$ace is not NULL
+      code <- input$ace
+      rstudioapi::insertText(code)
+      shiny::stopApp()  # Stop the Shiny app
+    })
     observeEvent(input$copyCode, {
       req(input$ace)  # Ensure input$ace is not NULL
       code <- input$ace
-      if (rstudioapi::isAvailable()) {
-        rstudioapi::insertText(code)
-      } else {
-        clipr::write_clip(code)
-        showNotification("Code copied to clipboard",
-                         type = "message")
-      }
-      if (rstudioapi::isAvailable()) {
-        shiny::stopApp()  # Stop the Shiny app
-      }
+      clipr::write_clip(code)
+      showNotification("Code copied to clipboard",
+                       type = "message")
     })
   })
 }
 
 aceUI <- function(id) {
   ns <- NS(id)
-  tagList(
-    fluidRow(
-      column(4,  actionButton(ns("copyCode"),
-                               ifelse(rstudioapi::isAvailable(),
-                                      "Insert & Exit",
-                                      "Copy"))),
-      column(4, {
-        selectInput(ns("theme"), NULL,
-                    shinyAce::getAceThemes(),
-                    selected="solarized_light",
-                    multiple=FALSE)
-      }),
-      column(4, {
-        sliderInput(ns("fontSize"),NULL,min=6,max=24,value=14,step=1)
-      })
-    ),
-    fluidRow(
-      shinyAce::aceEditor(
-        outputId = ns("ace"),
-        mode = "r",
-        # to access content of `selectionId` in server.R use `ace_selection`
-        # i.e., the outputId is prepended to the selectionId for use
-        # with Shiny modules
-        # selectionId = "selection",
-        value = "",
-        placeholder = "Model code will appear here...",
-        theme = "solarized_light",
-        fontSize = 14,
-        autoComplete = "live",
-        autoCompleters = "rlang"
-      )
-    )
-  )
+  if (rstudioapi::isAvailable()) {
+    tagList(
+      fluidRow(
+        column(2,  actionButton(ns("copyCode"), "Copy")),
+        column(2,  actionButton(ns("insertCode"), "Insert & Exit")),
+        column(4, {
+          selectInput(ns("theme"), NULL,
+                      shinyAce::getAceThemes(),
+                      selected="solarized_light",
+                      multiple=FALSE)
+        }),
+        column(4, {
+          sliderInput(ns("fontSize"),NULL,min=6,max=24,value=14,step=1)
+        })
+      ),
+      fluidRow(
+        shinyAce::aceEditor(
+          outputId = ns("ace"),
+          mode = "r",
+          # to access content of `selectionId` in server.R use `ace_selection`
+          # i.e., the outputId is prepended to the selectionId for use
+          # with Shiny modules
+          # selectionId = "selection",
+          value = "",
+          placeholder = "Model code will appear here...",
+          theme = "solarized_light",
+          fontSize = 14,
+          autoComplete = "live",
+          autoCompleters = "rlang")))
+  } else {
+    tagList(
+      fluidRow(
+        column(4,  actionButton(ns("copyCode"),
+                                ifelse(rstudioapi::isAvailable(),
+                                       "Insert & Exit",
+                                       "Copy"))),
+        column(4, {
+          selectInput(ns("theme"), NULL,
+                      shinyAce::getAceThemes(),
+                      selected="solarized_light",
+                      multiple=FALSE)
+        }),
+        column(4, {
+          sliderInput(ns("fontSize"),NULL,min=6,max=24,value=14,step=1)
+        })
+      ),
+      fluidRow(
+        shinyAce::aceEditor(
+          outputId = ns("ace"),
+          mode = "r",
+          # to access content of `selectionId` in server.R use `ace_selection`
+          # i.e., the outputId is prepended to the selectionId for use
+          # with Shiny modules
+          # selectionId = "selection",
+          value = "",
+          placeholder = "Model code will appear here...",
+          theme = "solarized_light",
+          fontSize = 14,
+          autoComplete = "live",
+          autoCompleters = "rlang")))
+  }
 }
